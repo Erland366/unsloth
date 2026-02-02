3e9d30b182150e498a8b3273c0a463e2633e9fdf
Erland366 <erland.pg366@gmail.com>
2026-02-02 22:27:10 +0000

    Change CUDA config to CUTLASS

--- unsloth/models/rl.py
+++ unsloth/models/rl.py
@@ -1098,8 +1098,8 @@ def _patch_trl_rl_trainers(trainer_file = "grpo_trainer"):
             "trace.enabled"     : False,
             #"combo_kernels"     : torch.cuda.get_device_capability()[0] >= 10,
             "triton.enable_persistent_tma_matmul": torch.cuda.get_device_capability()[0] >= 9,
-            "cuda.cutlass_epilogue_fusion_enabled": torch.cuda.get_device_capability()[0] >= 9, 
-            "cuda.cutlass_tma_only": torch.cuda.get_device_capability()[0] >= 9, 
+            "cutlass.cutlass_epilogue_fusion_enabled": torch.cuda.get_device_capability()[0] >= 9, 
+            "cutlass.cutlass_tma_only": torch.cuda.get_device_capability()[0] >= 9, 
             "cuda.compile_opt_level"              : "-O2",
             "cuda.enable_cuda_lto"                : True,
         }"""
